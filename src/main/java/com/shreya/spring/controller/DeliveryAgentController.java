cc043904a168ee1d9d783712ea4b7c3284179dbf
shreya mahalle <shreyamahalle2000@gmail.com>
2025-05-14 15:24:30 +0530

    updated

--- src/main/java/com/shreya/spring/controller/DeliveryAgentController.java
+++ src/main/java/com/shreya/spring/controller/DeliveryAgentController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("api/deliveryAgentManagement")
 @RequiredArgsConstructor
+
 public class DeliveryAgentController {
 
     private final Logger log = LoggerFactory.getLogger(DeliveryAgentController.class);

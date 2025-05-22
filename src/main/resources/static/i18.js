95098185fb64bb8cde195619d1d42d621fa2627c
shreya mahalle <shreyamahalle2000@gmail.com>
2025-05-22 12:50:27 +0530

    application properties added in i18n

--- src/main/resources/static/i18.js
+++ src/main/resources/static/i18.js
@@ -1,7 +1,17 @@
+//$(document).ready(function() {
+//    $("#locales").change(function () {
+//        var selectedOption = $('#locales').val();
+//        if (selectedOption != ''){
+//            window.location.replace('locale?lang=' + selectedOption);
+//        }
+//    });
+//});
+
+
 $(document).ready(function() {
     $("#locales").change(function () {
-        var selectedOption = $('#locales').val();
-        if (selectedOption != ''){
+        var selectedOption = $(this).val();  // Use 'this' instead of re-querying
+        if (selectedOption !== '') {
             window.location.replace('locale?lang=' + selectedOption);
         }
     });
